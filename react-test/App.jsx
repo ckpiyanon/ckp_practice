650f39f1a9dccb5fa0747dc47f337c7d9ded69f6
DaoDear <Dao_dear@hotmail.com>
2016-06-17 13:47:40 +0700

    finish react-test (to-do list)

--- react-test/App.jsx
+++ react-test/App.jsx
@@ -1,36 +1,49 @@
 import React from 'react';
 
+var idx = 0;
+
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			data: [
-				{"text":"Hello"},
-				{"text":"Hi"},
-				{"text":"Vola"}
-			]
-		}
-	}
 	render() {
 		return (
 			<div>
 				<InputGroup />
-				<ul className="list">
-					{this.state.data.map((d,i) => <ListItem key={i} item={d} />)}
-				</ul>
 			</div>
 		);
 	}
 }
 
 class InputGroup extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			data: [
+			]
+		}
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onClick = this.onClick.bind(this);
+	}
 	render() {
 		return (
 			<div>
-				<TextBox id="msg" />
-				<SubmitButton id="submit" />
+				<form onSubmit={this.onSubmit}>
+					<TextBox id="form_msg" />
+					<SubmitButton id="submit" />
+				</form>
+				<ul className="list">
+					{this.state.data.map((d,i) => <ListItem onClick={this.onClick} key={i} item={d} />)}
+				</ul>
 			</div>
 		);
+	}
+	onClick(e) {
+		this.setState({data: this.state.data.filter((d,i) => d.index !== e.target.id)});
+	}
+	onSubmit(e) {
+		e.preventDefault();
+		var ns = this.state.data.slice();
+		ns.push({"index": "msg" + (idx++),"text": document.getElementById("form_msg").value});
+		this.setState({data: ns});
+		document.getElementById("form_msg").value = "";
 	}
 }
 
@@ -42,14 +55,14 @@ class TextBox extends React.Component {
 
 class SubmitButton extends React.Component {
 	render() {
-		return (<button id={this.props.id}>SUBMIT</button>);
+		return (<button type="submit" id={this.props.id}>SUBMIT</button>);
 	}
 }
 
 class ListItem extends React.Component {
 	render() {
 		return (
-			<li>
+			<li onClick={this.props.onClick} id={this.props.item.index}>
 				{this.props.item.text}
 			</li>
 		);
