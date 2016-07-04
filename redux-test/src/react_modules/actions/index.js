c736dc5328b5f5e2f5b59ab4d0ff46aa57a8732d
Chinakrit Lorpiyanon <Chinakrit Lorpiyanon>
2016-07-04 12:08:51 +0700

    deleted const and put all codes in actions

--- redux-test/src/react_modules/actions/index.js
+++ redux-test/src/react_modules/actions/index.js
@@ -1,13 +1,13 @@
-export const addToCart = (itemId) => {
-	return {
-		type: 'ADD_TO_CART',
-		itemId
-	}
-}
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CHECKOUT = 'CHECKOUT';
 
+export const addToCart = (itemId) => {
+	return {type: ADD_TO_CART,itemId}
+}
 export const removeFromCart = (itemId) => {
-	return {
-		type: 'REMOVE_FROM_CART',
-		itemId
-	}
+	return {type: REMOVE_FROM_CART,itemId}
+}
+export const checkout = () => {
+	return {type: CHECKOUT}
 }
--- redux-test/src/react_modules/const/index.js
+++
@@ -1,9 +0,0 @@
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
-
-export function addToCart(id) {
-	return {type: ADD_TO_CART,id: id}
-}
-export function removeFromCart(id) {
-	return {type: REMOVE_FROM_CART,id: id}
-}
